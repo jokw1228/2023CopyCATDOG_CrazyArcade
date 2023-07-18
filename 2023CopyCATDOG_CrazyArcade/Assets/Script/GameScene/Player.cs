6948c5a3be01a10c04967a386fc37adb2a09d822
wjddbs1509 <wjddbs1509@gmail.com>
2023-07-18 23:14:41 +0900

    Last input priority, State enumaliztion, Needle script(restricted in player), Bush image

---
+++ 2023CopyCATDOG_CrazyArcade/Assets/Script/GameScene/Player.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    public GameObject waterbomb_prefab;                                 //물풍선 프리팹
+    public void GenerateWaterBomb(Vector2 position, int bomb_range)     //물풍선 생성 함수
+    {
+        GameObject bomb = Instantiate<GameObject>(waterbomb_prefab, position, Quaternion.identity);
+        bomb.GetComponent<WaterBomb>().bomb_range = bomb_range;
+    }
+
+    //변수들
+    public enum State
+    {
+        Playing=0, Imprisoned=1, Destroying=2, Immune=3
+    }
+
+    public State player_state = 0; //state
+
+    public int ballon_range = 1;
+    public int range_item = 0;
+
+    public int water_bomb_max = 1;
+    public int balloon_item = 0;
+
+    public const float basic_speed = 5;
+    public int speed_item = 0;
+    public float speed = basic_speed;
+
+    public const float speed_increase = 1;
+
+    public float ballon_timer = 0;
+    public int needle = 1;
+
+    public bool ballon_touched =false;
+    
+
+    public Vector2 pposition;
+    public Vector2Int pindex;
+    public TileInfo cur_tile_info = null;
+
+    // Update is called once per frame
+    /*void FixedUpdate()
+    {
+        if (player_state == 0)
+        {
+            speed = basic_speed + speed_item * speed_increase;
+            float horizontal = Input.GetAxisRaw("Horizontal"); //Axis계열로 받아오면 후입력 우선 불가
+            float vertical = Input.GetAxisRaw("Vertical");
+
+            if (Input.GetKeyDown(KeySetting.keys[KeyAction.UP1]))
+            {
+                Vector2 move = new Vector2(0, 1);
+                transform.Translate(move * Time.deltaTime * speed);
+            }
+            if (Input.GetKeyDown(KeySetting.keys[KeyAction.DOWN1]))
+            {
+                Vector2 move = new Vector2(0, -1);
+                transform.Translate(move * Time.deltaTime * speed);
+            }
+            if (Input.GetKeyDown(KeySetting.keys[KeyAction.RIGHT1]))
+            {
+                Vector2 move = new Vector2(1, 0);
+                transform.Translate(move * Time.deltaTime * speed);
+            }
+            if (Input.GetKeyDown(KeySetting.keys[KeyAction.LEFT1]))
+            {
+                Vector2 move = new Vector2(-1, 0);
+                transform.Translate(move * Time.deltaTime * speed);
+            }
+
+            pposition= MapManager.instance.GetClosestCellPosition(transform.position);
+            cur_tile_info = MapManager.instance.GetClosestTileInfo(transform.position);
+
+            if (Input.GetKeyDown(KeySetting.keys[KeyAction.BALL1]))
+            {
+                if (WaterBomb.num_of_cur_water_bomb < water_bomb_max + balloon_item)
+                    GenerateWaterBomb(pposition, ballon_range + range_item);//물풍선 오브젝트 생성<-ppostion+object 생성 명령어
+                else {
+                    //물폭탄이 필드에 최대 갯수만큼 설치되어 있을 경우의 코드
+                }
+            }
+
+            if (cur_tile_info.GetState(TileInfo.State.water_ray))
+            {
+               player_state = 1;
+                Debug.Log("Hit");
+            }
+            if(cur_tile_info.HaveItem())
+            {
+                cur_tile_info.UseItem(this);
+            }
+        }
+        else if (player_state == 1)//물풍선 감옥
+        {
+
+            ballon_timer += Time.deltaTime;
+
+            GetComponent<SpriteRenderer>().color = Color.blue;
+
+            if (ballon_touched==true) 
+            {
+                player_state = 2;
+            }
+            if (ballon_timer > 3)
+            {
+                player_state = 2;
+            }
+        }
+        else if (player_state == 2)//사망
+        {
+            Destroy(gameObject);
+        }
+    }*/
+
+    public void IncreaseWaterBombMax()
+    {
+        balloon_item++;
+    }
+    public void IncreaseWaterBombRange()
+    {
+        range_item++;
+    }
+    public void SpeedUp()
+    {
+        speed_item++;
+    }
+}
--- 2023CopyCATDOG_CrazyArcade/Assets/Script/GameScene/Player1.cs
+++ 2023CopyCATDOG_CrazyArcade/Assets/Script/GameScene/Player1.cs
@@ -2,118 +2,155 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class Player1 : MonoBehaviour
+public class Player1 : Player
 {
-    public GameObject waterbomb_prefab;
-    public void GenerateWaterBomb(Vector2 position, int bomb_range)
-    {
-        GameObject bomb = Instantiate<GameObject>(waterbomb_prefab, position, Quaternion.identity);
-        bomb.GetComponent<WaterBomb>().bomb_range = bomb_range;
-    }
-
-    //state
-    int player_state = 0;
-
-    int ballon_range = 1;
-    int range_item = 0;
-
-    int water_bomb_max = 1;
-    int balloon_item = 0;
-
-    const float basic_speed = 5;
-    int speed_item = 0;
-    float speed = basic_speed;
-    
-    const float speed_increase = 1;
-
-    float ballon_timer = 0;
-
-    bool ballon_touched =false;
 
     // Start is called before the first frame update
     void Start()
     {
-        Vector2 vector=new Vector2(0, 0);
+        Vector2 vector = new Vector2(0, 0);
     }
 
+    bool UP1_key = false;
+    bool DOWN1_key = false;
+    bool RIGHT1_key = false;
+    bool LEFT1_key = false;
 
-    Vector2 pposition;
-    Vector2Int pindex;
-    TileInfo cur_tile_info = null;
-
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if (player_state == 0)
+        if (player_state == State.Playing)          //플레이
         {
             speed = basic_speed + speed_item * speed_increase;
-            float horizontal = Input.GetAxisRaw("Horizontal"); //Axis계열로 받아오면 후입력 우선 불가
-            float vertical = Input.GetAxisRaw("Vertical");
-            if (horizontal != 0)
-            {
-                Vector2 move = new Vector2(horizontal, 0);
-                transform.Translate(move * Time.deltaTime * speed);
-            }
-            else if (vertical != 0)
-            {
-                Vector2 move = new Vector2(0, vertical);
-                transform.Translate(move * Time.deltaTime * speed);
-            }
-            pposition= MapManager.instance.GetClosestCellPosition(transform.position);
-            cur_tile_info = MapManager.instance.GetClosestTileInfo(transform.position);
 
-            if (Input.GetKeyDown(KeySetting.keys[KeyAction.BALL1]))
+            if (Input.GetKeyDown(KeySetting.keys[KeyAction.UP1]))       //UP1
+            {
+                UP1_key = true;
+                DOWN1_key = false;
+                RIGHT1_key = false;
+                LEFT1_key = false;
+            }
+            else if (Input.GetKeyUp(KeySetting.keys[KeyAction.UP1])){
+                UP1_key=false;
+            }
+
+            if (Input.GetKeyDown(KeySetting.keys[KeyAction.DOWN1]))       //DOWN1
+            {
+                UP1_key = false;
+                DOWN1_key = true;
+                RIGHT1_key = false;
+                LEFT1_key = false;
+            }
+            else if (Input.GetKeyUp(KeySetting.keys[KeyAction.DOWN1])){
+                DOWN1_key = false;
+            }
+
+            if (Input.GetKeyDown(KeySetting.keys[KeyAction.RIGHT1]))       //RIGHT1
+            {
+                UP1_key = false;
+                DOWN1_key = false;
+                RIGHT1_key = true;
+                LEFT1_key = false;
+            }
+            else if (Input.GetKeyUp(KeySetting.keys[KeyAction.RIGHT1])){
+                RIGHT1_key = false;
+            }
+
+            if (Input.GetKeyDown(KeySetting.keys[KeyAction.LEFT1]))      //LEFT1
+            {
+                UP1_key = false;
+                DOWN1_key = false;
+                RIGHT1_key = false;
+                LEFT1_key = true;
+            }
+            else if (Input.GetKeyUp(KeySetting.keys[KeyAction.LEFT1])){
+                LEFT1_key = false;
+            }
+
+            if (UP1_key)
+            {
+                Vector2 move = new Vector2(0, 1);
+                transform.Translate(move * Time.deltaTime * speed);
+            }
+            if (DOWN1_key)
+            {
+                Vector2 move = new Vector2(0, -1);
+                transform.Translate(move * Time.deltaTime * speed);
+            }
+            if (RIGHT1_key)
+            {
+                Vector2 move = new Vector2(1, 0);
+                transform.Translate(move * Time.deltaTime * speed);
+            }
+            if (LEFT1_key)
+            {
+                Vector2 move = new Vector2(-1, 0);
+                transform.Translate(move * Time.deltaTime * speed);
+            }
+
+            pposition = MapManager.instance.GetClosestCellPosition(transform.position); //player position -> pposition
+            cur_tile_info = MapManager.instance.GetClosestTileInfo(transform.position); //current tile information
+
+            if (Input.GetKeyDown(KeySetting.keys[KeyAction.BALL1]))                     // 물풍선 키 입력
             {
                 if (WaterBomb.num_of_cur_water_bomb < water_bomb_max + balloon_item)
-                    GenerateWaterBomb(pposition, ballon_range + range_item);//물풍선 오브젝트 생성<-ppostion+object 생성 명령어
-                else {
-                    //물폭탄이 필드에 최대 갯수만큼 설치되어 있을 경우의 코드
+                    GenerateWaterBomb(pposition, ballon_range + range_item);            //물풍선 오브젝트 생성<-ppostion+object 생성 명령어
+                else
+                {
+                                                                                        //물폭탄이 필드에 최대 갯수만큼 설치되어 있을 경우의 코드
                 }
             }
 
-            if (cur_tile_info.GetState(TileInfo.State.water_ray))
+            if (cur_tile_info.GetState(TileInfo.State.water_ray)) 
             {
-               player_state = 1;
+                player_state = State.Imprisoned;
                 Debug.Log("Hit");
             }
-            if(cur_tile_info.HaveItem())
+            if (cur_tile_info.HaveItem())
             {
                 cur_tile_info.UseItem(this);
             }
         }
-        else if (player_state == 1)//물풍선 감옥
+
+        else if (player_state == State.Imprisoned)  //물풍선 감옥
         {
 
             ballon_timer += Time.deltaTime;
 
             GetComponent<SpriteRenderer>().color = Color.blue;
 
-            if (ballon_touched==true) 
+            if ((Input.GetKeyDown(KeySetting.keys[KeyAction.ITEM1])) && needle >= 1) // case1: 탚출
             {
-                player_state = 2;
+                needle -= 1;
+                GetComponent<SpriteRenderer>().color = Color.white;
+                player_state = State.Immune;
+                ballon_timer = 0;
+            }
+
+            if (ballon_touched == true)                                             // case2: 사망
+            {
+                player_state = State.Destroying;
             }
             if (ballon_timer > 3)
             {
-                player_state = 2;
+                player_state = State.Destroying;
             }
         }
-        else if (player_state == 2)//사망
-        {
+
+        else if (player_state == State.Destroying)  //사망
+        { 
             Destroy(gameObject);
         }
-    }
 
-    public void IncreaseWaterBombMax()
-    {
-        balloon_item++;
-    }
-    public void IncreaseWaterBombRange()
-    {
-        range_item++;
-    }
-    public void SpeedUp()
-    {
-        speed_item++;
+        else if (player_state == State.Immune)      //무적=>물풍선 탈출 시 사용
+        {
+            ballon_timer += Time.deltaTime;
+
+            if (ballon_timer >= 1)
+            {
+                player_state = State.Playing;
+            }
+        }
+    
+
     }
 }
