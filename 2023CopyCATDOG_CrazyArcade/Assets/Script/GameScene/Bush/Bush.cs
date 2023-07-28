57b7f4cc8e510e353d967c89751ea4579f60ddb5
ysm436 <endtlr2004@gmail.com>
2023-07-29 01:32:50 +0900

    부쉬 판정 변경
    그림자를 제외한 Player의 자식 오브젝트(UFO 등) 또한 투명화 되도록 변경
    부쉬 콜라이더 크기 축소

--- 2023CopyCATDOG_CrazyArcade/Assets/Script/GameScene/Bush/Bush.cs
+++ 2023CopyCATDOG_CrazyArcade/Assets/Script/GameScene/Bush/Bush.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bush: MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         collision.gameObject.GetComponent<SpriteRenderer>().enabled = false; // -= new Color(0,0,0,1);
+        foreach( SpriteRenderer spr in collision.gameObject.GetComponentsInChildren<SpriteRenderer>())
+        {
+            if(spr.name != "Shadow")
+                spr.enabled = false;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         collision.gameObject.GetComponent<SpriteRenderer>().enabled = true; //+= new Color(0, 0, 0, 1);
+        foreach (SpriteRenderer spr in collision.gameObject.GetComponentsInChildren<SpriteRenderer>())
+        {
+            if (spr.name != "Shadow")
+                spr.enabled = true;
+        }
     }
 }
